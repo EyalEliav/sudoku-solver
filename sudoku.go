aa457a6bcea021bdfdb7cf1fcdf7b9ed30403044
EyalEliav <eyaleliav15@gmail.com>
2021-11-29 16:32:06 +0200

    fix errors

--- sudoku.go
+++ sudoku.go
@@ -4,8 +4,6 @@ import (
 	"math"
 )
 
-
-
 type Sudoku struct {
 	board [][]int
 	size  int
@@ -22,15 +20,15 @@ func NewSudoku(size int) *Sudoku {
 
 }
 
-func validateSequence(seq []int) bool{
+func validateSequence(seq []int) bool {
 	numMap := map[int]bool{}
-	for i:=1; i<=s.size;i++{
+	for i := 1; i <= len(seq); i++ {
 		numMap[i] = false
 	}
-	for _, value := range seq{
-		if value == 0{
+	for _, value := range seq {
+		if value == 0 {
 			continue
-		} else if numMap[value]{
+		} else if numMap[value] {
 			return false
 		} else {
 			numMap[value] = true
@@ -40,35 +38,36 @@ func validateSequence(seq []int) bool{
 
 }
 
-func (s Sudoku) validateCol(colInx int) bool{
+func (s Sudoku) validateCol(colInx int) bool {
 	col := make([]int, s.size)
-	for index, value := s.board{
+	for index, value := range s.board {
 		col[index] = value[colInx]
 	}
 	return validateSequence(col)
 
 }
 
-func (s Sudoku) validateRow(row int) bool{
-	return validateSequence(row)
+func (s Sudoku) validateRow(row int) bool {
+	return validateSequence(s.board[row])
 }
 
-func (s Sudoku) validateBlock(row int, col int){
+func (s Sudoku) validateBlock(row int, col int) bool {
 	block := make([]int, s.size)
 	index := 0
-	root = int(math.Sqrt(float64(s.size)))
-	for i:=(row/root)*root; i < ((row/root)+1)*root; i++{
-		for j:=(col/root)*root; i < ((col/root)+1)*root; i++, index++{
+	root := int(math.Sqrt(float64(s.size)))
+	for i := (row / root) * root; i < ((row/root)+1)*root; i++ {
+		for j := (col / root) * root; i < ((col/root)+1)*root; i++ {
 			block[index] = s.board[i][j]
+			index++
 		}
 	}
 	return validateSequence(block)
 }
 
-func (s Sudoku) Solve{
+func (s Sudoku) Solve() {
 
 }
 
-func (s Sudoku) Print(){
+func (s Sudoku) Print() {
 
 }
